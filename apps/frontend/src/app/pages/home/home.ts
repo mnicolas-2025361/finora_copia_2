f851bbc220e02117337db41be018559609681a76
mnicolas-2025361 <mnicolas-2025361@kinal.edu.gt>
2026-08-31 01:32:17 -0600

    feat: se agrega html y css del dashboard según el documento

--- apps/frontend/src/app/pages/home/home.ts
+++ apps/frontend/src/app/pages/home/home.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements AfterViewInit {
+
+  presupuesto = 3500;
+  gastado = 2183.87;
+
+  ngAfterViewInit(): void {
+
+    const disponible = this.presupuesto - this.gastado;
+
+    new Chart('presupuestoChart', {
+      type: 'doughnut',
+
+      data: {
+        labels: ['Gastado', 'Disponible'],
+
+        datasets: [{
+          data: [this.gastado, disponible],
+
+          backgroundColor: [
+            '#4fd1a1',
+            '#9b6de3'
+          ],
+
+          borderWidth: 0
+        }]
+      },
+
+      options: {
+        cutout: '65%',
+
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+
+  }
+}
