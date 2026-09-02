549e1e0cde59e8584a8caddf9ce2c9b3900c7c31
mnicolas-2025361 <mnicolas-2025361@kinal.edu.gt>
2026-09-02 00:33:10 -0600

    feat: se crea carpeta de ingresos con sus respectivos archivos

---
+++ apps/frontend/src/app/pages/ingresos/ingresos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresos',
+  imports: [],
+  templateUrl: './ingresos.html',
+  styleUrl: './ingresos.css',
+})
+export class Ingresos {}
